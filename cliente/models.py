f8707c67765127133eafa560f706e56ae5343b43
ismael de sousa <83679151+ismael0821@users.noreply.github.com>
2022-06-22 23:18:44 -0300

    Add files via upload

---
+++ cliente/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Cliente(models.Model):
+
+    dataCadrastro = models.DateField(max_length=100)
+
+    def __str__(self):
+
+        return self.Consultar_imovel
+    def __str__(self):
+
+        return self.Manter_conta
+    def __str__(self):
+
+        return self.Agendar_visita
